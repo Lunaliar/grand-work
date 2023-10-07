d410670c8b7f93e369e88b468574e620268cabf1
Lunaliar <savcostabile@gmail.com>
2023-10-07 11:12:42 -0700

    Daily commit: 2023-10-07

--- commiter.sh
+++ commiter.sh
@@ -3,13 +3,12 @@
 # Set your GitHub username and repository information
 GITHUB_USERNAME="Lunaliar"
 REPO_NAME="grand-work"
-BRANCH_NAME="main"  # Or your preferred branch name
+BRANCH_NAME="main"  
 
-# Generate the content for the commit (replace this with your content)
+# Generate the content for commit
 CONTENT="Pay no attention to that man behind the curtain!"
-
-# Navigate to your repository directory
-
+#Navigate to directory
+cd ~/Code/practice/grand-work
 
 # Update the repository to avoid conflicts
 git pull origin $BRANCH_NAME
