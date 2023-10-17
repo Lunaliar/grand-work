0fbab77e84091151c1b0a9b8488047d548ead962
Lunaliar <savcostabile@gmail.com>
2023-10-17 16:00:01 -0700

    Daily commit: 2023-10-17

---
+++ Resume.js
@@ -0,0 +1,11 @@
+const contactData = {
+	name: 'Saverio Costabile',
+	phoneNumber: '(434) 409-4506',
+	email: 'savcostabile@gmail.com',
+	location: 'Vancouver, WA',
+};
+const aboutData = {
+	about:
+		'Highly motivated React developer with a strong passion for web development. Leveraging a background in customer service, I bring exceptional communication skills, teamwork, and leadership experience to every project. Proficient in HTML/CSS/JavaScript, I have successfully deployed a Facebook Messenger clone website and further honed my expertise in React JavaScript and Firebase. I thrive on utilizing third-party packages and exploring innovative styling frameworks. Check out my portfolio at https://savcodes.dev to see more of my work.',
+	skills: ['React', 'HTML', 'JavaScript', 'CSS', 'Node.js'],
+};
